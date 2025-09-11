17355d8134ff91fa33a4154e5b78cc9b1c9d88b1
Aleksander Stanuch <aleksanderstanuch@gmail.com>
2025-09-11 18:11:19 +0200

    Add basic .fasta files reading functionality.

---
+++ src/fasta_read.py
@@ -0,0 +1,7 @@
+from Bio import SeqIO
+
+def read_fasta(file_path):
+    sequences = []
+    for record in SeqIO.parse(file_path, "fasta"):
+        sequences.append(str(record.seq))
+    return sequences
--- src/main.py
+++ src/main.py
@@ -1,14 +1,29 @@
-# Enter, read and save DNA Sequence 1
+import os
+from fasta_read import read_fasta
+from Bio import SeqIO
 
-seq1_filename = input("Enter the name of sequence 1 file (without file format): ")
+def get_file_path(base_dir, filename):
+    seq_dir = os.path.join(base_dir, "..", "sequences") # fasta files path
+    return os.path.join(seq_dir, f"{filename}.fasta")
 
-with open(f"../sequences/{seq1_filename}.txt") as seq1:
-    sequence1 = seq1.read()
+def print_sequences(file_path, label):
+    print(f"{label}:")
+    for record in SeqIO.parse(file_path, "fasta"):
+        print(f"ID: {record.id}")
+        print(f"Seqence: {record.seq}\n")
 
+def main():
+    base_dir = os.path.dirname(os.path.abspath(__file__)) # main.py path
 
-# Enter, read and save DNA Sequence 2
+    seq1_filename = input("Enter the name of sequence 1 file (without extension): ")
+    seq2_filename = input("Enter the name of sequence 2 file (without extension): ")
+    print("")
 
-seq2_filename = input("Enter the name of sequence 2 file (without file format): ")
+    seq1_path = get_file_path(base_dir, seq1_filename)
+    seq2_path = get_file_path(base_dir, seq2_filename)
 
-with open(f"../sequences/{seq2_filename}.txt") as seq2:
-    sequence2 = seq2.read()
+    print_sequences(seq1_path, seq1_filename)
+    print_sequences(seq2_path, seq2_filename)
+
+if __name__ == "__main__":
+    main()
